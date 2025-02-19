097fee4b08877fb4fc3d2c219111f538b36ced1c
Ali Najafi K <pureAliNajafi@gmail.com>
2025-02-19 17:32:52 +0330

    score filter part 1️⃣ searchparams

--- src/app/(others)/flying-machines/page.tsx
+++ src/app/(others)/flying-machines/page.tsx
@@ -3,13 +3,19 @@ import { getFlyingMachines } from "@/lib/api";
 import MachineCard from "@/components/MachineCard";
 import Pagination from "@/components/Pagination";
 import { FlyingMachineSearchParams, Machine } from "@/lib/types";
+import ScoreFilter from "@/components/ScoreFilter";
 
 export default async function Page({ searchParams }: { searchParams: FlyingMachineSearchParams }) {
   const flyingMachines = await getFlyingMachines(searchParams);
   // console.log(flyingMachines.data);
   return (
     <div className="grid grid-cols-12">
-      <section className="col-span-3 bg-gray-100 p-5">SideBar</section>
+      <section className="col-span-3 bg-gray-100 p-5">
+        <label>Attributes</label>
+        {["Attack", "Defense", "Speed", "Agility", "Capacity"].map((attr) => (
+          <ScoreFilter key={attr} attr={attr} />
+        ))}
+      </section>
       <section className="col-span-9 p-5">
         <div className="grid grid-cols-3 gap-5">
           {/* <pre>{JSON.stringify(flyingMachines.data, null, 2)}</pre> */}
---
+++ src/components/ScoreFilter.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { ChangeEvent } from "react";
+
+const ScoreFilter = ({ attr }: { attr: string }) => {
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const { replace } = useRouter();
+
+  const selectedScore = searchParams.get(attr);
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    const params = new URLSearchParams(searchParams);
+    if (!value) return;
+    if (value === "-1") {
+      params.delete(attr);
+      replace(`${pathName}?${params.toString()}`);
+      return;
+    }
+
+    params.set(attr, value);
+    replace(`${pathName}?${params.toString()}`);
+  };
+  return (
+    <div>
+      <label>{attr}</label>
+      <select
+        title="filterAttrValue"
+        onChange={handleChange}
+        value={selectedScore ? parseInt(selectedScore) : undefined}
+      >
+        <option value={-1}></option>
+        {[5, 4, 3, 2, 1].map((op) => (
+          <option key={op} value={op}>
+            {op}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default ScoreFilter;
