45b16e17724ad8500fbe7204ffcb3dde66977220
Ali Najafi K <pureAliNajafi@gmail.com>
2025-03-06 13:13:48 +0330

    made puplic route in strapi ,no need to use token for public reqs

--- src/core/services/apiClient.ts
+++ src/core/services/apiClient.ts
@@ -5,7 +5,7 @@ const API_URL = process.env.STRAPI_API_URL;
 export const http = axios.create({
   baseURL: API_URL,
   headers: {
-    Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
+    // Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
     "Content-Type": "application/json",
   },
 });
